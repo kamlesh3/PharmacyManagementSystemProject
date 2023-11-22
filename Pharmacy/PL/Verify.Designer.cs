f4f6dea4cffea0b9a2d7a550531d806269f4b45d
Kamlesh Gaderiya <kamlesh88125@gmail.com>
2023-11-22 10:42:20 +0000

    Add All file of project
    Final all code modification done.

---
+++ Pharmacy/PL/Verify.Designer.cs
@@ -0,0 +1,160 @@
+﻿using System;
+
+namespace Pharmacy.PL
+{
+    partial class Verify
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.otp = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textOtp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(154, 40);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(271, 22);
+            this.textEmail.TabIndex = 0;
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.Location = new System.Drawing.Point(67, 40);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(41, 16);
+            this.email.TabIndex = 1;
+            this.email.Text = "Email";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(67, 92);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(67, 16);
+            this.password.TabIndex = 2;
+            this.password.Text = "Password";
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(154, 92);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(271, 22);
+            this.textPassword.TabIndex = 3;
+            // 
+            // otp
+            // 
+            this.otp.AutoSize = true;
+            this.otp.Location = new System.Drawing.Point(70, 140);
+            this.otp.Name = "otp";
+            this.otp.Size = new System.Drawing.Size(35, 16);
+            this.otp.TabIndex = 5;
+            this.otp.Text = "OTP";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(238, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(594, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Verify OTP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(479, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Send OTP";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textOtp
+            // 
+            this.textOtp.Location = new System.Drawing.Point(154, 141);
+            this.textOtp.Name = "textOtp";
+            this.textOtp.Size = new System.Drawing.Size(271, 22);
+            this.textOtp.TabIndex = 9;
+            // 
+            // Verify
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textOtp);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.otp);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.textEmail);
+            this.Name = "Verify";
+            this.Load += new System.EventHandler(this.Verify_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.Label email;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label otp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label wrong;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textOtp;
+    }
+}
---
+++ Pharmacy/PL/Verify.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Pharmacy.BL;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace Pharmacy.PL
+{
+    public partial class Verify : MetroFramework.Forms.MetroForm
+    {
+        FRM_MAIN frm = new FRM_MAIN();
+        FRM_Cashier Cashier = new FRM_Cashier();
+        string randomCode;
+        bool flag=false;
+        public Verify()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            DataTable dt = Login.login(textEmail.Text, textPassword.Text);
+            if (dt.Rows.Count > 0)
+            {
+                
+               
+                Program.UserFullName = dt.Rows[0]["U_Full_Name"].ToString();
+                Program.Permision = dt.Rows[0]["Per_ID"].ToString();
+                
+                BL.Logs.Logs_Add(Program.UserFullName, DateTime.Now.ToString(), "Login");
+
+                this.Hide();
+                if(flag == true)
+                {
+                    if (dt.Rows[0]["Per_ID"].ToString() == "3")
+                    {
+                        Cashier.Show();
+                    }
+                    else
+                    {
+                        frm.ShowDialog();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Code");
+                }
+               
+            }
+            else
+            {
+                wrong.Text = "invalid password or name";
+                textEmail.Clear();
+                textPassword.Clear();
+                textOtp.Clear();
+                textEmail.Focus();
+            }
+
+
+
+        }
+
+        private void emailSend(string toemail)
+        {
+            var fromMail = "pharmacysystemautomail@gmail.com";
+            var fromPwd = "tfoulgaxgkpzrnbw";
+
+            Random rand = new Random();
+            randomCode = (rand.Next(999999)).ToString();
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(fromMail);
+            message.Subject = "User Verification Mail";
+            message.To.Add(new MailAddress(toemail));
+            message.Body = "Your OTP: " + randomCode;
+            message.IsBodyHtml = true;
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPwd),
+                EnableSsl = true,
+            };
+
+            try
+            {
+                smtpClient.Send(message);
+                MessageBox.Show("Confirmation Email sent successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void verifyOtp()
+        {
+            
+            
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void txtPass_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textPassword.Text))
+            {
+                e.Cancel = true;
+                textEmail.Focus();
+                errorProvider1.SetError(textPassword, "Please enter your name!");
+                errorProvider1.SetIconPadding(textPassword, -20);
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(textPassword, null);
+            }
+        }
+
+        private void txtName_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textEmail.Text))
+            {
+                e.Cancel = true;
+                textEmail.Focus();
+                errorProvider1.SetError(textEmail, "Please enter your name!");
+                errorProvider1.SetIconPadding(textEmail, -20);
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(textEmail, null);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string otp=textOtp.Text.Trim().ToString();
+            if (randomCode==otp)
+            {
+                flag = true;
+                MessageBox.Show("OTP verified successfully");
+            }
+            else
+            {
+                MessageBox.Show("OPT not verified " + randomCode + " "+ otp +" " + (randomCode==otp));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string Email="";
+            string pass="";
+            if (textEmail.Text == string.Empty)
+            {
+                textEmail.Focus();
+                return;
+            }
+            if (textPassword.Text == string.Empty)
+            {
+                textPassword.Focus();
+                return;
+            }
+            try
+            {
+                DataTable dt = Login.login(textEmail.Text, textPassword.Text);
+                Email = dt.Rows[0]["U_Name"].ToString();
+                pass = dt.Rows[0]["U_Pass"].ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(textEmail.Text.ToString()+" is not registered email");
+            }
+
+
+            if (Email==textEmail.Text.ToString())
+            {
+                emailSend(textEmail.Text.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Please enter correct email");
+            }
+        }
+        private void Verify_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
